57032ba5afd4679420c8c2f5969a5cf864a75d35
thomas.jungbauer@lonza.com <thomas.jungbauer@lonza.com>
2024-12-23 14:47:21 +0100

    .

--- vars/jiraCreateIssue.groovy
+++ vars/jiraCreateIssue.groovy
@@ -1,34 +1,18 @@
 def call(Map config=[:]) {
-    withCredentials([usernamePassword(credentialsId: 'jira-user-token', passwordVariable: 'JIRA_PW', usernameVariable: 'JIRA_USER')]) {
 
-        sh """
-cat <<EOF > createIssue.json
-{
-   "fields": {
-      "project": {
-         "key": "${config.JIRA_KEY}"
-      },
-      "summary": "${config.JIRA_SUMMARY}",
-      "description": "${config.JIRA_DESCRIPTION}",
-      "issuetype": {
-         "name": "${config.JIRA_ISSUE_TYPE}"
-      },
-      "assignee": {
-         "name": "assignee-username"
-      }
-   }
-}
-EOF
+    def CURRENT_DATE = sh(script: "date +%Y%m%d", returnStdout: true).trim()
+
+  sh """
+
+echo "Today: ${CURRENT_DATE}"
+echo "Creating new Jira Ticket: "
+
+curl -k --request POST --url 'https://${config.JIRA_URL}/rest/api/2/issue' \
+  -H 'Authorization: Bearer ${config.JIRA_TOKEN}' \
+  --header 'Accept: application/json' \
+  --header 'Content-Type: application/json' \
+  --data '{"fields": {"project":{"key": "${config.JIRA_PROJECT}"},"summary": "${config.JIRA_PROJECT_SUMMARY} - ${CURRENT_DATE}","description": "${config.JIRA_PROJECT_DESCRIPTION}","issuetype": {"name": "${config.JIRA_ISSUE_TYPE}"}}}'
+
         """
-
-
-        //def jiraToken="${JIRA_USER}:${JIRA_PW}"
-        sh """
-        ls -la
-        cat createIssue.json
-        curl -D- -o createIssueResult.json -u "${JIRA_USER}:${JIRA_PW}" -X POST --data @createIssue.json -H "Content-Type: application/json" ${config.JIRA_URL}/rest/api/2/issue
-        """
-        archiveArtifacts artifacts: '*.*', followSymlinks: false
-    }
-
 }
+
